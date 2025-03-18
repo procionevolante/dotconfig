9d29630736abdfed9ed551e6cf0a60fba0d49482
Andrea Fanti <s276160@studenti.polito.it>
2025-03-18 12:19:18 +0100

    gdb: nxp-k3: add MC_ME,FLASH,PFLASH,CAN and CRC peripherals

--- gdb/nxp-s32k3-memreg.py
+++ gdb/nxp-s32k3-memreg.py
@@ -32,12 +32,29 @@ _regs = [
     NxpReg(0x4046C000, 'swt1', 'SWT_Type'),
     # Interrupt Router CP (core2core irq generation/status)
     NxpReg(0x40260200, 'ircp', 'IPC_MSCM_IRCPnIRx_Type'),
-    # Reset Generation Module (to trigger reset or see reset cause)
-    NxpReg(0x4028C000, 'rgm', 'MC_RGM_Type'),
+    # MC_ peripherals
+    NxpReg(0x4028C000, 'rgm', 'MC_RGM_Type'), # Reset Generation Module
+    NxpReg(0x402DC000, 'me', 'MC_ME_Type'), # Mode Entry (cpu cores pwr manager)
     # pins I/O - System Integration Unit Lite2 (SIUL2)
     # With an enum going like A00 = 0, A31 = 31, B00 = 32, ...
     # offsets of pin-wide registers w.r.t. A00 is ((pin XOR 3) - 3)
     NxpReg(0x40290000, 'siul2', 'SIUL2_Type'),
+    # memories
+    NxpReg(0x402EC000, 'flash', 'FLASH_Type'),
+    NxpReg(0x40268000, 'pflash0', 'PFLASH_Type'),
+    # communication
+    NxpReg(0x40304000, 'can0', 'FLEXCAN_Type'),
+    NxpReg(0x40308000, 'can1', 'FLEXCAN_Type'),
+    NxpReg(0x4030C000, 'can2', 'FLEXCAN_Type'),
+    NxpReg(0x40310000, 'can3', 'FLEXCAN_Type'),
+    NxpReg(0x40314000, 'can4', 'FLEXCAN_Type'),
+    NxpReg(0x40318000, 'can5', 'FLEXCAN_Type'),
+    NxpReg(0x4031C000, 'can6', 'FLEXCAN_Type'),
+    NxpReg(0x40320000, 'can7', 'FLEXCAN_Type'),
+    NxpReg(0x40570000, 'can8', 'FLEXCAN_Type'),
+    NxpReg(0x40574000, 'can9', 'FLEXCAN_Type'),
+    # misc
+    NxpReg(0x40380000, 'crc', 'CRC_Type'),
 ]
 
 for reg in _regs:
