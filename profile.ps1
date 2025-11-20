3503fe072439f7dcebbed0d4c144d07ca9d51982
Andrea FANTI <s276160@studenti.polito.it>
2025-11-20 14:14:08 +0100

    add powershell init script

---
+++ profile.ps1
@@ -0,0 +1,9 @@
+#!/usr/bin/env powershell
+#
+# Powershell init script
+# Check possible locations via `$PROFILE | Select-Object *`
+#
+
+# use readline/emacs/bash keybindings
+# github.com/PowerShell/PSReadLine
+Set-PSReadLineOption -EditMode Emacs
